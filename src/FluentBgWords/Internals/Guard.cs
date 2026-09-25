9d6c6fe610f51b9bcdf37364917754c5e219c512
aleksandarMilev <aleksandarmilev23@gmail.com>
2026-09-25 21:37:17 +0300

    fix: validate custom unit and currency values (BUG-01)

---
+++ src/FluentBgWords/Internals/Guard.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentBgWords.Internals;
+
+internal static class Guard
+{
+    public static string Text(
+        string value,
+        string paramName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value, paramName);
+
+        if (char.IsWhiteSpace(value[0]) || char.IsWhiteSpace(value[^1]))
+        {
+            throw new ArgumentException(
+                "Value must not have leading or trailing whitespace.",
+                paramName);
+        }
+
+        return value;
+    }
+
+    public static T NotNull<T>(
+        T value,
+        string paramName)
+        where T : class
+    {
+        ArgumentNullException.ThrowIfNull(value, paramName);
+        return value;
+    }
+}
